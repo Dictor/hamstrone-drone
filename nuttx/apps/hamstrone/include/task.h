b6390956da2eb1b551f7ed1f36e6906e86b94966
김정현 <kimdictor@gmail.com>
2021-01-03 12:17:09 +0900

    Tidy up header

--- nuttx/apps/hamstrone/include/task.h
+++ nuttx/apps/hamstrone/include/task.h
@@ -1,16 +1,21 @@
 #ifndef HAMSTRONE_TASK_VALUE_H
 #define HAMSTRONE_TASK_VALUE_H
 
+/* Standard header */
 #include <time.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/* Hamstrone header */
 #include "hamstrone_main.h"
 #include "hamster_tongue.h"
 #include "config.h"
 #include "serialize.h"
-#include <stdlib.h>
-#include <time.h>
 #include "value_store.h"
+
+/* Nuttx header */
+#include "nuttx/i2c/i2c_master.h"
 
 int tskTransmitValue(int argc, char * argv []);
 int tskUpdateValue(int argc, char *argv[]);
