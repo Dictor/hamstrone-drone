aede1a06c90508665b9da0c376953d9d9c80dfe9
Jeonghyun Kim <kimdictor@gmail.com>
2021-02-04 00:50:20 +0900

    Add debugf general function and config about it

--- include/hamster_tongue.h
+++ include/hamster_tongue.h
@@ -13,7 +13,6 @@
 /* Hamstrone header */
 #include "config.h"
 
-
 /* MESSAGE STRUCTURE */
 #define HAMSTERTONGUE_MESSAGE_MARKER 0xFF
 
@@ -27,6 +26,8 @@
 #define HAMSTERTONGUE_MESSAGE_NOUN_SIGNAL_INITFAIL 0x01
 #define HAMSTERTONGUE_MESSAGE_NOUN_SIGNAL_I2CREADFAIL 0x02
 #define HAMSTERTONGUE_MESSAGE_NOUN_SIGNAL_DEBUG 0x03
+
+#define HAMSTERTONGUE_GENERAL_BUFFER_MULTIPLY 5
 
 /*
 message serialize format: 
@@ -46,6 +47,7 @@ typedef struct _HAMSTERTONGUE_Message {
 //sem_t* WriteSemaphore;
 
 void HAMSTERTONGUE_SetWriteSemaphore(sem_t* sem);
+void HAMSTERTONGUE_SetDefaultFile(int fd);
 HAMSTERTONGUE_Message* HAMSTERTONGUE_NewMessage(uint8_t verb, uint8_t noun, uint8_t payloadLength);
 HAMSTERTONGUE_Message* HAMSTERTONGUE_NewStringMessage(uint8_t verb, uint8_t noun, char* str);
 HAMSTERTONGUE_Message* HAMSTERTONGUE_NewFormatStringMessage(uint8_t verb, uint8_t noun, int bufSize, char* format, ...);
@@ -53,4 +55,5 @@ uint16_t HAMSTERTONGUE_GetMessageLength(HAMSTERTONGUE_Message* msg);
 uint8_t* HAMSTERTONGUE_SerializeMessage(HAMSTERTONGUE_Message* msg);
 ssize_t HAMSTERTONGUE_WriteMessage(int fd, HAMSTERTONGUE_Message* msg);
 ssize_t HAMSTERTONGUE_WriteAndFreeMessage(int fd, HAMSTERTONGUE_Message* msg);
+ssize_t HAMSTERTONGUE_Debugf(char *format, ...);
 #endif
