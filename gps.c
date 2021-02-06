e36b57a34e543296b2fc64a57b8ed1ee0f5116f1
PlutoNH <namjonghyen@naver.com>
2021-02-07 00:19:07 +0900

    Summing Functions

--- gps.c
+++ gps.c
@@ -1,86 +1,66 @@
 #include "include/gps.h"
 
-void GPS_type_GGA(char * Data_receive, struct Ele_Num GGAN)
+void GPS_type(char * dataReceive, struct Ele_Num gpsType, char * type)
 {
 	int commaCnt=0, i, j;
-	for(i=0;i<strlen(Data_receive);i++)
+	for(i=0;i<strlen(dataReceive);i++)
 	{
-		if(Data_receive[i]==',')
+		if(dataReceive[i]==',')
 			commaCnt++;	
 	}
-	if(commaCnt!=14)
+	if(strcmp(type,"GGA")==0 && commaCnt==14)
 	{
-		HAMSTERTONGUE_Debugf("Data was damagegd\n");
-	}
-	else
-	{
-		for(i=0;i<GGAN.num-1;i++)
+		for(i=0;i<gpsType.num-1;i++)
 		{
 			if(i==5||i==6||i==7||i==9)
 			{
-				for(j=GGAN.Element[i];j<GGAN.Element[i+1]-1;j++)
-					HAMSTERTONGUE_Debugf("%c", Data_receive[j]);
+				for(j=gpsType.Element[i];j<gpsType.Element[i+1]-1;j++)
+					printf("%c", dataReceive[j]);
 			}
-			HAMSTERTONGUE_Debugf("\n");
+			printf("\n");
 		}
 	}
-}
-
-void GPS_type_GLL(char * Data_receive,struct Ele_Num GLLN)
-{
-	int commaCnt=0, i, j;
-	for(i=0;i<strlen(Data_receive);i++)
+	else if(strcmp(type,"GLL")==0 && commaCnt==7)
 	{
-		if(Data_receive[i]==',')
-			commaCnt++;	
-	}
-	if(commaCnt!=7)
-	{
-		HAMSTERTONGUE_Debugf("Data was damagegd\n");
-	}
-	else
-	{
-		for(i=0;i<GLLN.num-1;i++)
+		for(i=0;i<gpsType.num-1;i++)
 		{
 			if(i==1||i==2||i==3||i==4)
 			{
-				for(j=GLLN.Element[i];j<GLLN.Element[i+1]-1;j++)
-					HAMSTERTONGUE_Debugf("%c", Data_receive[j]);
+				for(j=gpsType.Element[i];j<gpsType.Element[i+1]-1;j++)
+					printf("%c", dataReceive[j]);
 			}
-			HAMSTERTONGUE_Debugf("\n");
+			printf("\n");
 		}
 	}
+	else
+		printf("Data was damaged\n");
 }
 
-void Split(char *Data_receive)
+void Split(char *dataReceive)
 {
     int dataCnt = 0, eleCnt = 1, i=0, start, len=0;
-    char GPS_Type[4];
+    char gpsType[4];
     struct Ele_Num Ele;
     Ele.Element[0]=0;
     while (1)
     {
-        if (Data_receive[dataCnt] == ',')
+        if (dataReceive[dataCnt] == ',')
         {
             Ele.Element[eleCnt] = dataCnt + 1;
             eleCnt++;
-            if(Data_receive[dataCnt+1]==',')
+            if(dataReceive[dataCnt+1]==',')
             	dataCnt++;
         }
 		if(dataCnt==2 || dataCnt==3 || dataCnt==4)
 		{
-			GPS_Type[i]=Data_receive[dataCnt];
+			gpsType[i]=dataReceive[dataCnt];
 			i++;
 		}
         len++;
-        if(len==strlen(Data_receive))
+        if(len==strlen(dataReceive))
         	break;
         dataCnt++;
     }
 	Ele.num = eleCnt;
-	HAMSTERTONGUE_Debugf("parser calling, elecnt=%d", Ele.num);
-	if(strcmp(GPS_Type,"GGA")==0)
-		GPS_type_GGA(Data_receive,Ele);		
-	else if(strcmp(GPS_Type,"GLL")==0)
-		GPS_type_GLL(Data_receive,Ele);
+	GPS_type(dataReceive,Ele,gpsType);
 }
